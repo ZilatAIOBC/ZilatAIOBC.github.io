a96bbdd36e8e8736f2f7c51796ac2d8ceb7e3180
ZilatAIOBC <146693454+ZilatAIOBC@users.noreply.github.com>
2025-06-11 23:30:05 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,34 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/ingest/static/:path*",
-  //       destination: "https://us-assets.i.posthog.com/static/:path*",
-  //     },
-  //     {
-  //       source: "/ingest/:path*",
-  //       destination: "https://us.i.posthog.com/:path*",
-  //     },
-  //     {
-  //       source: "/ingest/decide",
-  //       destination: "https://us.i.posthog.com/decide",
-  //     },
-  //   ];
-  // },
+// const nextConfig: NextConfig = {
+//   // async rewrites() {
+//   //   return [
+//   //     {
+//   //       source: "/ingest/static/:path*",
+//   //       destination: "https://us-assets.i.posthog.com/static/:path*",
+//   //     },
+//   //     {
+//   //       source: "/ingest/:path*",
+//   //       destination: "https://us.i.posthog.com/:path*",
+//   //     },
+//   //     {
+//   //       source: "/ingest/decide",
+//   //       destination: "https://us.i.posthog.com/decide",
+//   //     },
+//   //   ];
+//   // },
+//   output: "export",
+//   // This is required to support PostHog trailing slash API requests
+//   // skipTrailingSlashRedirect: true,
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   output: "export",
-  // This is required to support PostHog trailing slash API requests
-  // skipTrailingSlashRedirect: true,
-};
+  trailingSlash: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
